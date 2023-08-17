44417d25993dfbd83ace000f3c1cc1d7fdd8cbb8
Chitradevi Maruthavanan <chitram2@pdx.edu>
2023-08-17 14:19:12 -0700

    image controller optimizing variable names

--- backend/controllers/imageController.js
+++ backend/controllers/imageController.js
@@ -1,44 +1,43 @@
 const { Butler } = require("butler-sdk");
+
 const apiKey = process.env.APIKEY;
 const queueId = process.env.QUEUEID;
 
 const client = new Butler(apiKey);
 const fs = require("fs");
 var multer = require("multer");
-const upload = multer({ dest: 'img/'});
 
 exports.scanReceipt = (req, res) => {
-  let array = [];
-  let arrayTemprory = [];
-  let resp = [];
-  if (req.file.path && typeof req.file.path != undefined) {
-    const file = fs.createReadStream(req.file.path);
+  let extractedData = [];
+  let tempDataArray = [];
+  let responseArray = [];
+
+  if (req.file.path && typeof req.file.path !== undefined) {
+    const fileStream = fs.createReadStream(req.file.path);
 
     client
-      .extractDocument(queueId, file)
+      .extractDocument(queueId, fileStream)
       .then((response) => {
         console.log(response);
-        //res.send(response);
-        let items = response.tables[0].rows;
+        const items = response.tables[0].rows;
 
-        items.forEach((ele, ind) => {
-          ele.cells.forEach((ele1, ind1) => {
-            array.push({ name: ele1.columnName, value: ele1.value });
+        items.forEach((row) => {
+          row.cells.forEach((cell) => {
+            extractedData.push({ name: cell.columnName, value: cell.value });
           });
-          arrayTemprory.push(array);
-          array = [];
+          tempDataArray.push(extractedData);
+          extractedData = [];
         });
 
-        arrayTemprory.forEach((ele, ind) => {
-          resp.push({
-            name: ele.find((todo) => todo.name == "Item Name").value,
-            quantity: ele.find((todo) => todo.name == "Quantity").value,
-            // unitprice: ele.find((todo) => todo.name == "Unit Price").value,
-            value: ele.find((todo) => todo.name == "Total Price").value,
+        tempDataArray.forEach((data) => {
+          responseArray.push({
+            name: data.find((item) => item.name === "Item Name").value,
+            quantity: data.find((item) => item.name === "Quantity").value,
+            value: data.find((item) => item.name === "Total Price").value,
           });
         });
-        console.log(resp);
-        res.status(200).send(resp);
+        console.log(responseArray);
+        res.status(200).send(responseArray);
       })
       .catch((error) => {
         console.log(error);
@@ -48,9 +47,9 @@ exports.scanReceipt = (req, res) => {
   }
 };
 
-exports.uploadreceipt = (req, res) => {
+exports.uploadReceipt = (req, res) => {
   console.log(req.body);
-  res.send("Image has been uploaded here : " + req.file.path);
+  res.send("Image has been uploaded here: " + req.file.path);
 };
 
 exports.imageStorage = multer.diskStorage({
@@ -61,3 +60,5 @@ exports.imageStorage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
+
+
