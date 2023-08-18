aad6f032de7a1c84ea2ab33887fc8ecac8975f47
Chitradevi Maruthavanan <chitram2@pdx.edu>
2023-08-17 22:15:53 -0700

    optimize the enhanced email test code readability"

--- backend/test/emailController.test.js
+++ backend/test/emailController.test.js
@@ -1,29 +1,31 @@
-let chai = require("chai");
-let chaiHttp = require("chai-http");
-let server = require("../server");
-let should = chai.should();
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../server");
+const should = chai.should();
 chai.use(chaiHttp);
 
-let requestData2 = {
+const requestData = {
   email: "tejesh6965@gmail.com",
   string: "hi this is a test string",
 };
-describe("Post request to Email sending function", () => {
-  // mocking an environment
-  before(function () {
-    env = process.env;
+
+describe("POST request to Email sending function", () => {
+  // Mocking the environment
+  const originalEnv = process.env;
+  before(() => {
     process.env = {
       EMAIL_AUTH: "dharamthokpranav@gmail.com",
       EMAIL_PASSWORD: "wrectzhamtdiqasj",
       EMAIL_SENDER_ADDRESS: "customerservice@clicksplit.com",
     };
   });
-  it("it should return a response", async () => {
+
+  it("should return a response", async () => {
     await chai
       .request(server)
       .post("/clicknsplit/api/send-email")
-      .send(requestData2)
-      .then(function (res) {
+      .send(requestData)
+      .then((res) => {
         should.exist(res.status);
         res.should.have.status(200);
         res.body.should.have.property("accepted");
@@ -31,8 +33,9 @@ describe("Post request to Email sending function", () => {
       });
   });
 
-  // restoring everything back
-  after(function () {
-    process.env = env;
+  // Restoring the original environment
+  after(() => {
+    process.env = originalEnv;
   });
 });
+
