4f81071e01c7db4b66566e276715a48218f4eb6e
harshita-jaiswal <harshu.jaiswal26@gmail.com>
2023-08-19 00:15:12 -0700

    fix eslint issues in selection.js

--- frontend/src/Pages/selection/selection.js
+++ frontend/src/Pages/selection/selection.js
@@ -26,7 +26,7 @@ function Selection() {
   };
 
   const buildColumns = () => {
-    return getColumns().map((column) => <th>{column}</th>);
+    return getColumns().map((column, ind) => <th key={ind}>{column}</th>);
   };
 
   const buildSingleRow = (row) => {
@@ -38,10 +38,9 @@ function Selection() {
       temp.push(
         <td>
           <input
-            onChange={onCheckboxChange}
+            onChange={(e) => onCheckboxChange(e, row.value)}
             id={`${row.name}${currentIndividual}`}
             name={row.name}
-            data_total_value={row.value}
             checked={
               !!selectionData?.[`${row.name}`]?.people_involved.includes(
                 currentIndividual
@@ -57,7 +56,7 @@ function Selection() {
   const buildRows = () => {
     let temp = [];
     tableData.forEach((row = {}, index) => {
-      temp.push(<tr>{buildSingleRow(row)}</tr>);
+      temp.push(<tr key={index}>{buildSingleRow(row)}</tr>);
     });
     return temp;
   };
@@ -105,10 +104,11 @@ function Selection() {
     setCurrentIndividual(e.target.value);
   };
 
-  const onCheckboxChange = (e) => {
+  const onCheckboxChange = (e, value) => {
     let data = Object.assign({}, selectionData);
     const { name, checked } = e.target;
-    const totalValue = e.target.getAttribute("data_total_value");
+    const totalValue = value;
+    // const totalValue = e.target.getAttribute("data_total_value") + "hello";
     if (data[name]) {
       console.log("old");
       if (!checked) {
@@ -140,24 +140,6 @@ function Selection() {
   }, []);
 
   return (
-    // <div>
-    //     <p>Select the person from the dropdown and select tick mark in front of item if person has bought that
-    //         item</p>
-    //     <div>
-    //         <select onChange={onIndividualChange} value={null}>
-    //             <option value=""></option>
-    //             {personsList.map(person => {
-    //                 return <option value={person}>{person}</option>
-    //             })}
-    //         </select>
-    //     </div>
-    //     {tableData.length && buildTable()}
-    //     {/*<Link to={'contri'}>*/}
-    //     <button onClick={createFinalSelectionData}>
-    //         Done
-    //     </button>
-    //     {/*</Link>*/}
-    // </div>
     <section className="text-center">
       <div
         className="card mx-4 mx-md-5 shadow-5-strong"
@@ -176,8 +158,8 @@ function Selection() {
             <div className="d-flex align-content-end mb-3">
               <select onChange={onIndividualChange} value={null}>
                 <option value=""></option>
-                {personsList.map((person) => {
-                  return <option value={person}>{person}</option>;
+                {personsList.map((person, index) => {
+                  return <option key={index} value={person}>{person}</option>;
                 })}
               </select>
             </div>
