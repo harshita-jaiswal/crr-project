ee560cc1ad0d36b551f9cea73d612d1fa3b3dd7d
Sarvesh Biradar <sbiradar@pdx.edu>
2023-08-20 18:27:27 +0530

    converted App from class to functional component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import "./assets/css/App.css";
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import Homepage from "./Pages/Homepage/Homepage";
 import TablePage from "./Pages/Table/Table";
 import Upload from "./Pages/Upload/Upload";
@@ -8,33 +8,25 @@ import Selection from "./Pages/Selection/Selection";
 import Contribution from "./Pages/Contribution/Contribution";
 import image from "./assets/img/bill.jpg";
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <Router>
-        <div className="main_div">
-          <header className="App-header">
-            <h1 className="App">Click-n-Split</h1>
-          </header>
-          <div
-            className="p-5 bg-image img-fluid"
-            style={{
-              backgroundImage: `url(${image})`,
-              height: "500px",
-            }}
-          ></div>
-          <main>
-            <Route path="/" exact component={Homepage} />
-            <Route path="/upload" exact component={Upload} />
-            <Route path="/review" exact component={TablePage} />
-            <Route path="/selection" exact component={Selection} />
-            <Route path="/contributions" exact component={Contribution} />
-          </main>
-          <main className="App"></main>
-        </div>
-      </Router>
+        <Router>
+            <div className="main_div">
+                <header className="App-header">
+                    <h1 className="App">Click-n-Split</h1>
+                </header>
+                <div className="p-5 bg-image img-fluid"
+                     style={{backgroundImage: `url(${image})`, height: "500px"}}></div>
+                <main>
+                    <Route path="/" exact component={Homepage}/>
+                    <Route path="/upload" exact component={Upload}/>
+                    <Route path="/review" exact component={TablePage}/>
+                    <Route path="/selection" exact component={Selection}/>
+                    <Route path="/contributions" exact component={Contribution}/>
+                </main>
+            </div>
+        </Router>
     );
-  }
 }
 
 export default App;
