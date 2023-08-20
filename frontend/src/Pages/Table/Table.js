c6f3f5ed688c9377d70ff85b876d247dbbc08a38
Sarvesh Biradar <sbiradar@pdx.edu>
2023-08-20 19:44:08 +0530

    added semantic html and comments. made code more readable.

--- frontend/src/Pages/Table/Table.js
+++ frontend/src/Pages/Table/Table.js
@@ -1,88 +1,84 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
+/**
+ * Renders a detailed table of purchased items.
+ * Data is retrieved from local storage.
+ * @returns {JSX.Element}
+ */
 function TablePage() {
   const [data, setData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [rows, setRows] = useState([]);
-  const [col, setCol] = useState([]);
-  useEffect(() => {
-    let data = JSON.parse(localStorage.getItem("tableData"));
-    setData(data);
-    setIsLoaded(true);
 
+  useEffect(() => {
+    // Load data from local storage
+    const storedData = JSON.parse(localStorage.getItem("tableData"));
+    setData(storedData);
+    setIsLoaded(true);
   }, []);
 
-  const getColumns = () => {
-    let temp = [];
-    for (const [key, value] of Object.entries(data[0])) {
-      temp.push(key.toLocaleUpperCase());
-    }
-    return temp;
-  };
+    const getColumnHeadings = () => {
+        let columnHeadings = [];
+        for (const [heading] of Object.entries(data[0])) { //keys of any entry object in data array are column headings
+            columnHeadings.push(heading.toLocaleUpperCase());
+        }
+        return columnHeadings;
+    };
 
   const buildColumns = () => {
-    return getColumns().map((column) => <th>{column}</th>);
+    return getColumnHeadings().map((column, i) => <th key={i}>{column}</th>);
   };
 
   const buildSingleRow = (row) => {
-    let temp = [];
-    for (const [key, value] of Object.entries(row)) {
-      temp.push(<td>{value}</td>);
-    }
-    return temp;
+    return Object.values(row).map((value, i) => <td key={i}>{value}</td>);
   };
 
   const buildRows = () => {
-    let temp = [];
-    data.forEach((row = {}, index) => {
-      temp.push(<tr>{buildSingleRow(row)}</tr>);
-    });
-    return temp;
+    return data.map((row, index) => <tr key={index}>{buildSingleRow(row)}</tr>);
   };
 
   const buildTable = () => {
     return (
-      <table>
-        <tr>{buildColumns()}</tr>
-        {buildRows()}
-      </table>
+        <table>
+          <thead>
+          <tr>{buildColumns()}</tr>
+          </thead>
+          <tbody>{buildRows()}</tbody>
+        </table>
     );
   };
 
   return (
-   
-    <section className="text-center">
-      <div
-        className="card mx-4 mx-md-5 shadow-5-strong"
-        style={{
-          marginTop: "-400px",
-          background: "hsla(0, 0%, 100%, 0.8)",
-          backdropFilter: "blur(30px)",
-        }}
-      >
-        <div className="card-body py-5 px-md-5">
-          <div className="row d-flex justify-content-center">
-            <h3>Review your purchases</h3>
+      <section className="text-center">
+        <div
+            className="card mx-4 mx-md-5 shadow-5-strong"
+            style={{
+              marginTop: "-400px",
+              background: "hsla(0, 0%, 100%, 0.8)",
+              backdropFilter: "blur(30px)",
+            }}
+        >
+          <div className="card-body py-5 px-md-5">
             <div className="row d-flex justify-content-center">
-              {!isLoaded && <p>...Loading</p>}
-              {isLoaded && buildTable()}
-            </div>
-            <div className="row mt-3">
-              <div className="buttonTable">
-                <Link to={"upload"}>
-                  <button className="btn btn-info btn-sml">{`< Retry`}</button>
-                </Link>
-                <Link to={"selection"}>
-                  <button className="btn btn-info btn-sml">{`Done! >`}</button>
-                </Link>
+              <h3>Review Your Purchases</h3>
+              <div className="row d-flex justify-content-center">
+                {!isLoaded && <p>Loading...</p>}
+                {isLoaded && buildTable()}
+              </div>
+              <div className="row mt-3">
+                <div className="buttonTable">
+                  <Link to="upload">
+                    <button className="btn btn-info btn-sml">&lt; Retry</button>
+                  </Link>
+                  <Link to="selection">
+                    <button className="btn btn-info btn-sml">Done! &gt;</button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
 
